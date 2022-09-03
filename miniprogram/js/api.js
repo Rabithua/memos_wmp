40690f7dacce04d158c81bcc3c331bcadece6ac6
Rabithua <rabithua@gmail.com>
2022-09-03 21:45:34 +0800

    修正

--- miniprogram/js/api.js
+++ miniprogram/js/api.js
@@ -64,7 +64,7 @@ export const editMemo = (url, openId, memoId,data) => {
       success(res) {
         resolve(res.data)
       },
-      fail(err) {s
+      fail(err) {
         reject(err)
       }
     })
