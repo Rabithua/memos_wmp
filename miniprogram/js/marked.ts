d7922624a14dc6ddd70e94e4e4550d119762143e
Rabithua <rabithua@gmail.com>
2023-03-08 12:08:41 +0800

    feat: 解析引用内容，正则表达式太难了

--- miniprogram/js/marked.ts
+++ miniprogram/js/marked.ts
@@ -8,35 +8,37 @@ const BOLD_TEXT_REG = /\*\*(.+?)\*\*/g;
 const EM_TEXT_REG = /\*(.+?)\*/g;
 const TODO_BLOCK_REG = /- \[ \] (.+?)(\n|$)/g;
 const DONE_BLOCK_REG = /- \[x\] (.+?)(\n|$)/g;
-const DOT_LI_REG = /[^|\n]( *)[*-] (.+?)(\n|$)/g;
-const NUM_LI_REG = /[^|\n]( *\d+)\. (.+?)(\n|$)/g;
+const DOT_LI_REG = /[^|\n]*( *)[*-] (.+?)(\n|$)/g;
+const NUM_LI_REG = /[^|\n]*( *\d+)\. (.+?)(\n|$)/g;
 const TAG_REG = /#([^\s#]+)/g;
 const IMAGE_URL_REG = /!\[.*?\]\(http(.+?)\)/g;
 const IMAGE_LOCAL_URL_REG = /!\[.*?\]\((.+?)\)/g;
 const LINK_URL_REG = /\[(.*?)\]\((.+?)\)/g;
 const MEMO_LINK_REG = /@\[(.+?)\]\((.+?)\)/g;
 const TITLE_TEXT_REG = /(^|\n)#{1,6}\s(.+)/g;
+const BLOCKQUOTE_REG = /[\n]*> (.+?)(?:\r?\n|$)/g;
 
 const parseMarkedToHtml = (markedStr: string): string => {
   const htmlText = markedStr
     // 注释自动在英文和中文间添加空格的正则
     // .replace(/([\u4e00-\u9fa5])([A-Za-z0-9?.,;[\]]+)/g, "$1 $2")
     // .replace(/([A-Za-z0-9?.,;[\]]+)([\u4e00-\u9fa5])/g, "$1 $2")
-    .replace(CODE_BLOCK_REG, "<pre class='code' lang=''>$1</pre>")
+    .replace(BLOCKQUOTE_REG, "\n<div class='quote'>$1</div>\n")
+    .replace(CODE_BLOCK_REG, "\n<pre class='code' lang=''>$1</pre>\n")
     .replace(SHORT_CODE_BLOCK_REG, "<pre class='shortCode' lang=''>$1</pre>")
-    .replace(TODO_BLOCK_REG, "<div class='listDiv'><span class='todo-block todo' data-value='TODO'></span><span class='todo-text'>$1</span><br></div>")
-    .replace(DONE_BLOCK_REG, "<div class='listDiv'><span class='todo-block done' data-value='DONE'></span><span class='todo-text todo-text-done'>$1</span><br></div>")
-    .replace(DOT_LI_REG, "<div class='counter-block'><div class='dotlist-dot'>$1•</div><div class='dotlist-content'>$2</div></div>")
-    .replace(NUM_LI_REG, "<div class='counter-block'><div class='orderlist-dot'>$1.</div><div class='orderlist-content'>$2</div></div>")
+    .replace(TODO_BLOCK_REG, "\n<div class='listDiv'><span class='todo-block todo' data-value='TODO'></span><span class='todo-text'>$1</span><br></div>\n")
+    .replace(DONE_BLOCK_REG, "\n<div class='listDiv'><span class='todo-block done' data-value='DONE'></span><span class='todo-text todo-text-done'>$1</span><br></div>\n")
+    .replace(DOT_LI_REG, "\n<div class='counter-block'><div class='dotlist-dot'>$1•</div><div class='dotlist-content'>$2</div></div>\n")
+    .replace(NUM_LI_REG, "\n<div class='counter-block'><div class='orderlist-dot'>$1.</div><div class='orderlist-content'>$2</div></div>\n")
     .replace(BOLD_TEXT_REG, "<strong>$1</strong>")
     .replace(EM_TEXT_REG, "<em>$1</em>")
     .replace(TITLE_TEXT_REG, (match, offset) => {
       let res = TITLE_TEXT_REG.exec(match)
-      console.log(TITLE_TEXT_REG.exec(match))
+      // console.log(TITLE_TEXT_REG.exec(match))
       if (res) {
-        console.log(res)
+        // console.log(res)
         let num = res[0].length - res[1].length - res[2].length - 1
-        return '<h' + num + " class='md_title' style='margin: 0;' >" + res[2] + '</h' + num + '>';
+        return '\n<h' + num + " style='margin: 10px 0;' >" + res[2] + '</h' + num + '>\n';
       } else {
         return ''
       }
